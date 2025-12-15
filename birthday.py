c3881daf6bbd06b690fa37f7a5fc623ebfc1b81c
Sudhish Narayan S <ssndb134@gmail.com>
2025-12-15 23:27:10 +0530

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -74,7 +74,22 @@ if st.button("Click for surprise", use_container_width=True):
 ████████████████████████████████████████████████████████
 ]
 """
-	st.markdown(mdd, unsafe_allow_html=False)
+	#st.markdown(mdd, unsafe_allow_html=False)
+
+st.markdown(
+    f"""
+    <pre style="
+        font-family: monospace;
+        font-size: 18px;
+        line-height: 1.2;
+        color: #4DA8FF;
+        white-space: pre;
+    ">
+{mdd}
+    </pre>
+    """,
+    unsafe_allow_html=True
+)
 	st.audio("audio.mp3", autoplay=True)
 
 
